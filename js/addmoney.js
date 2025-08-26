9777eeb66d1e81772e17950f98db98ee05a5a599
Nayeem Hasan Shanto <snayeemhasan@gmail.com>
2025-08-26 17:22:42 +0600

    add money features successfully done

--- js/addmoney.js
+++ js/addmoney.js
@@ -1,5 +1,23 @@
-console.log("file connected")
-document.getElementById('add-money-btn').addEventListener('click', function(e){
-    e.preventDefault();
-    console.log("button worked");
+console.log("file connected");
+
+document.getElementById('add-money').addEventListener('click', function(e){
+     e.preventDefault();
+    // console.log("button worked");
+    const availableBalance = parseInt(document.getElementById('available-balance').innerText);
+    const selectBank = document.getElementById('select-bank');
+    const accountNumber =document.getElementById('account-number').value;
+    const addAmount = parseInt(document.getElementById('add-amount').value);
+    const pinNumber = parseInt(document.getElementById('pin-number').value);
+
+    // console.log(availableBalance,selectBank,accountNumber,addAmount,pinNumber);
+    const defaultPinNumber = 3842;
+    if(accountNumber.length < 11){
+        alert("Please provide a valid account number");
+    }
+    if(defaultPinNumber != pinNumber){
+        alert("Please provide the valid pin number")
+        return;
+    }
+    const newBalance = addAmount+availableBalance;
+    document.getElementById('available-balance').innerText=newBalance;
 })
