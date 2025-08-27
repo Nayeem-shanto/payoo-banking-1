3fc82678a856d68041616a1f3f43157034d0dc1e
Nayeem Hasan Shanto <snayeemhasan@gmail.com>
2025-08-27 22:20:24 +0600

    add money using function

--- js/home.js
+++ js/home.js
@@ -1,37 +1,56 @@
-document.getElementById('logout').addEventListener('click',function(){
-    window.location.href='index.html';
-})
+document.getElementById("logout").addEventListener("click", function () {
+  window.location.href = "index.html";
+});
 
-/* document.getElementById('add-money').addEventListener('click', function(e){
-    e.preventDefault();
-    document.getElementById('add-money-container').style.display = 'block';
-    document.getElementById('cashout-container').style.display   = 'none';
-    document.getElementById('transfer-money-container').style.display = 'none';
-    document.getElementById('get-bonus-container').style.display = 'none';
-    document.getElementById('pay-bill-container').style.display = 'none';
-    document.getElementById('transaction-container').style.display = 'none';
+/* Features for add money form */
 
-}) */
+/* function for all inner text */
+function getInnerText(id){
+    const amount = parseInt(document.getElementById(id).innerText);
+    return amount;
+}
+/* function for all input value */
+function getInputValue(id){
+    const inputValue = document.getElementById(id).value;
+    return inputValue;
+}
+/* function for all input value converted in integer */
+
+function getInputValueConverted(id){
+    const inputValueConverted = parseInt(document.getElementById(id).value);
+    return inputValueConverted;
+}
 
 
-/* 
-document.getElementById('cashout').addEventListener('click', function(e){
+document
+  .getElementById("add-money-submit-btn")
+  .addEventListener("click", function (e) {
     e.preventDefault();
-    window.location.href = 'cashOut.html';
-})
-document.getElementById('transfer-money').addEventListener('click', function(e){
-    e.preventDefault();
-    window.location.href = 'transferMoney.html';
-})
-document.getElementById('get-bonus').addEventListener('click', function(e){
-    e.preventDefault();
-    window.location.href = 'getBonus.html';
-})
-document.getElementById('pay-bill').addEventListener('click', function(e){
-    e.preventDefault();
-    window.location.href = 'payBill.html';
-})
-document.getElementById('transaction').addEventListener('click', function(e){
-    e.preventDefault();
-    window.location.href = 'transactions.html';
-}) */
+    const defaultPinNumber = 3842;
+    const initialBalance = getInnerText('initial-balance');
+    console.log(initialBalance);
+    const bankSelection = getInputValue('select-bank');
+    console.log(bankSelection);
+    const bankAccountNumber = getInputValue('bank-account-number');
+    console.log(bankAccountNumber);
+
+    const addAmount = getInputValueConverted('add-amount');
+    console.log(addAmount)
+
+    const pinNumber = getInputValueConverted('pin-number');
+    console.log(pinNumber);
+
+    if(bankAccountNumber.length !=11){
+        alert("Please Provide a valid account number");
+        return;
+    }
+
+    if(pinNumber != defaultPinNumber){
+        alert("please provide a valid pin number");
+    }
+    const newBalance = addAmount + initialBalance;
+    document.getElementById('initial-balance').innerText= newBalance;
+
+
+
+  });
